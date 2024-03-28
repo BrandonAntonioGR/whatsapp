acec2e0f8ea67be62c73dcb2c30defff19ddc453
BrandonAntonioGR <163066758+BrandonAntonioGR@users.noreply.github.com>
2024-03-27 21:48:15 -0600

    Envia msg de whatsapp desde frame

---
+++ whatsapp/src/main/java/com/mycompany/whatsapp/Whatsapp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.whatsapp;
+
+/**
+ *
+ * @author mkpol
+ */
+public class Whatsapp {
+
+    public static void main(String[] args) {
+        PantallaWhatsapp pw= new PantallaWhatsapp();
+        pw.setVisible(true);
+        pw.setLocationRelativeTo(null);
+    }
+}
